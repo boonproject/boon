85eb83871d75cdb1930e9b551284952ceae3299f
Richard Hightower <richardhightower@gmail.com>
2013-12-14 02:59:30 -0800

    full

--- src/test/java/org/boon/json/PlistTest.java
+++ src/test/java/org/boon/json/PlistTest.java
@@ -6,7 +6,6 @@ import org.junit.Test;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 
 import static org.boon.Exceptions.die;
 import static org.boon.Maps.idx;
