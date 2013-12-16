fb2013c6d8b4a992fadf8837ae7be8cc29d76edb
Richard Hightower <richardhightower@gmail.com>
2013-12-16 01:51:45 -0800

    stop overriding user defined charset

--- src/main/java/org/boon/json/JsonParserFactory.java
+++ src/main/java/org/boon/json/JsonParserFactory.java
@@ -9,90 +9,86 @@ public class JsonParserFactory {
     boolean preferCharSequence = false;
 
     boolean useDirectBytes = true;
-    Charset charset= null;
+    Charset charset = null;
     boolean overlay = false;
     int sizeToUseOverlay = 50;
     boolean lax;
     private boolean pllistStyle;
 
 
-    public JsonParserFactory lax() {
+    public JsonParserFactory lax () {
         lax = true;
         return this;
     }
 
-    public JsonParserFactory strict() {
+    public JsonParserFactory strict () {
         lax = false;
         return this;
     }
 
-    public JsonParserFactory useDirectBytes() {
-         useDirectBytes = true;
+    public JsonParserFactory useDirectBytes () {
+        useDirectBytes = true;
         return this;
     }
 
 
-    public JsonParserFactory neverUseDirectBytes() {
+    public JsonParserFactory neverUseDirectBytes () {
         useDirectBytes = false;
         return this;
     }
 
 
-
-    public JsonParserFactory setCharset( Charset charset ) {
+    public JsonParserFactory setCharset ( Charset charset ) {
         this.charset = charset;
         return this;
     }
 
 
-    public JsonParserFactory setSizeToUseOverlay( int size ) {
+    public JsonParserFactory setSizeToUseOverlay ( int size ) {
         this.sizeToUseOverlay = size;
         return this;
     }
 
-    public JsonParserFactory preferCharSequence(  ) {
+    public JsonParserFactory preferCharSequence () {
         this.preferCharSequence = true;
         return this;
     }
 
 
-
-    public JsonParserFactory neverPreferCharSequence(  ) {
+    public JsonParserFactory neverPreferCharSequence () {
         this.preferCharSequence = false;
         return this;
     }
 
-    public JsonParserFactory useOverlay() {
-         overlay = true;
+    public JsonParserFactory useOverlay () {
+        overlay = true;
         return this;
     }
 
 
-    public JsonParserFactory neverUseOverlay() {
+    public JsonParserFactory neverUseOverlay () {
         overlay = false;
         return this;
     }
 
 
-
-    public JsonParser create() {
-
+    public JsonParser create () {
 
 
-        if (lax && pllistStyle && charset == null || charset == StandardCharsets.US_ASCII) {
+        if ( lax && pllistStyle && charset == null || charset == StandardCharsets.US_ASCII ) {
             this.useDirectBytes = true;
 
         }
 
-         if ( ( charset==null ) && lax && pllistStyle) {
+        if ( ( charset == null ) && lax && pllistStyle ) {
 
-             charset = StandardCharsets.US_ASCII;
-         }else {
-             charset = StandardCharsets.UTF_8;
-         }
+            charset = StandardCharsets.US_ASCII;
+        } else if ( charset == null ) {
+            charset = StandardCharsets.UTF_8;
+        }
 
         return new JsonParserImpl ( useDirectBytes, charset, overlay, sizeToUseOverlay,
-         preferCharSequence, lax, pllistStyle );
+                preferCharSequence, lax, pllistStyle );
     }
 
 
