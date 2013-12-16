e410349424f0e8dc022f0bbe9c0a5a65d91663ef
Christopher Rohrbaugh <Chris.Rohrbaugh@ascentis.com>
2013-12-16 10:29:43 -0800

    Made MessageHolder public

--- src/main/java/org/boon/validation/RecursiveDescentPropertyValidator.java
+++ src/main/java/org/boon/validation/RecursiveDescentPropertyValidator.java
@@ -17,7 +17,7 @@ import java.util.*;
 public class RecursiveDescentPropertyValidator {
     protected ValidatorMetaDataReader validatorMetaDataReader = new AnnotationValidatorMetaDataReader ( );
 
-    protected class MessageHolder {
+    public class MessageHolder {
         public final String propertyPath;
         public final ValidatorMessageHolder holder;
 
