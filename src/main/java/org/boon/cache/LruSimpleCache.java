76868fe0db792fc87bfa8d4bc3674d74d6e83b37
Richard Hightower <richardhightower@gmail.com>
2013-12-18 13:57:49 -0800

    simple cache

---
+++ src/main/java/org/boon/cache/LruSimpleCache.java
@@ -0,0 +1,57 @@
+    package org.boon.cache;
+
+
+
+    import java.util.LinkedHashMap;
+    import java.util.Map;
+
+    public class LruSimpleCache<K, V> implements LruCache <K, V>{
+
+        Map<K, V> map = new LinkedHashMap (  );
+
+
+        public LruSimpleCache (final int limit) {
+               map = new LinkedHashMap <K, V> (16, 0.75f, true) {
+                   @Override
+                   protected boolean removeEldestEntry(final Map.Entry<K, V> eldest) {
+                       return super.size() > limit;
+                   }
+               };
+        }
+        @Override
+        public void put ( K key, V value ) {
+            map.put ( key, value );
+        }
+
+        @Override
+        public V get ( K key ) {
+            return map.get(key);
+        }
+
+        //For testing only
+        @Override
+        public V getSilent ( K key ) {
+            V value =  map.get ( key );
+            if (value!=null) {
+                map.remove ( key );
+                map.put(key, value);
+            }
+            return value;
+        }
+
+        @Override
+        public void remove ( K key ) {
+            map.remove ( key );
+        }
+
+        @Override
+        public int size () {
+            return map.size ();
+        }
+
+        public String toString() {
+            return map.toString ();
+        }
+
+
+    }
---
+++ src/test/java/org/boon/cache/SimpleConcurrentLRUCache.java
@@ -0,0 +1,93 @@
+    package org.boon.cache;
+
+    import org.junit.Test;
+
+    import static org.boon.Boon.puts;
+    import static org.boon.Exceptions.die;
+
+
+    public class SimpleConcurrentLRUCache {
+
+
+        @Test
+        public void test () {
+            LruCache <Integer, Integer> cache = new LruSimpleConcurrentCache<> ( 1, 4, false );
+
+
+            cache.put ( 0, 0 );
+            cache.put ( 1, 1 );
+
+            cache.put ( 2, 2 );
+            cache.put ( 3, 3 );
+
+
+            boolean ok = cache.size () == 4 || die ( "size" + cache.size () );
+
+
+            cache.put ( 4, 4 );
+            cache.put ( 5, 5 );
+
+            puts (cache);
+            ok |= cache.size () == 4 || die ( "size" + cache.size () );
+            ok |= cache.getSilent ( 2 ) == 2 || die ();
+            ok |= cache.getSilent ( 3 ) == 3 || die ();
+            ok |= cache.getSilent ( 4 ) == 4 || die ();
+            ok |= cache.getSilent ( 5 ) == 5 || die ();
+
+
+            cache.get ( 2 );
+            cache.get ( 3 );
+            cache.put ( 6, 6 );
+            cache.put ( 7, 7 );
+            ok |= cache.size () == 4 || die ( "size" + cache.size () );
+            ok |= cache.getSilent ( 2 ) == 2 || die ();
+            ok |= cache.getSilent ( 3 ) == 3 || die ();
+
+            cache.put ( 8, 8 );
+            cache.put ( 9, 9 );
+
+            ok |= cache.getSilent ( 4 ) == null || die ();
+            ok |= cache.getSilent ( 5 ) == null || die ();
+
+
+            puts (cache);
+
+
+            if ( !ok ) die ();
+
+        }
+
+
+        @Test
+        public void test2 () {
+            LruCache <Integer, Integer> cache = new LruSimpleConcurrentCache<> ( 400, false );
+
+
+            cache.put ( 0, 0 );
+            cache.put ( 1, 1 );
+
+            cache.put ( 2, 2 );
+            cache.put ( 3, 3 );
+
+
+            for (int index =0 ; index < 5_000; index++) {
+                cache.get(0);
+                cache.get ( 1 );
+                cache.put ( 2, index  );
+                cache.put ( 3, index );
+                cache.put(index, index);
+            }
+
+            boolean ok = cache.getSilent ( 0 ) == 0 || die ();
+            ok |= cache.getSilent ( 1 ) == 1 || die ();
+            ok |= cache.getSilent ( 2 ) != null || die ();
+            ok |= cache.getSilent ( 3 ) != null || die ();
+
+            ok |= cache.size () < 600 || die();
+            if ( !ok ) die ();
+
+
+
+        }
+
+    }
