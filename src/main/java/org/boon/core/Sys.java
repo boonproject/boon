d8b7f5bd6871d305074b4096901cda3d605cb7ed
Richard Hightower <richardhightower@gmail.com>
2014-01-04 12:08:00 -0800

    disable FastStringUtils if not JDK1.7 or later

--- src/main/java/org/boon/core/Sys.java
+++ src/main/java/org/boon/core/Sys.java
@@ -7,30 +7,115 @@ import org.boon.core.reflection.Reflection;
 import org.boon.core.timer.TimeKeeper;
 import org.boon.core.timer.TimeKeeperBasic;
 
+import java.math.BigDecimal;
 import java.util.concurrent.atomic.AtomicReference;
+
 
 public class Sys {
 
     private final static boolean isWindows;
     private final static boolean inContainer;
-
-
-    public static void println( String message ) {
-        System.out.println( message );
-    }
-
-    public static void print( String message ) {
-        System.out.print( message );
-    }
-
-
-    public static boolean isWindows() {
-        return isWindows;
-    }
+    private final static boolean is1_7OorLater;
+    private final static int buildNumber;
+    private final static BigDecimal version;
+    private final static boolean is1_7;
+    private final static boolean is1_8;
 
 
     static {
-        if ( System.getProperty( "os.name" ).contains( "Windows" ) ) {
+        BigDecimal v = new BigDecimal ( "-1" );
+        int b = -1;
+        String sversion = System.getProperty ( "java.version" );
+        if ( sversion.indexOf ( "_" ) != -1 ) {
+            final String[] split = sversion.split ( "_" );
+            try {
+
+                String ver = split [0];
+                if (ver.startsWith ( "1.8" )) {
+                    v = new BigDecimal ("1.8" );
+                }
+                if (ver.startsWith ( "1.7" )) {
+                    v = new BigDecimal ("1.7" );
+                }
+
+                if (ver.startsWith ( "1.6" )) {
+                    v = new BigDecimal ("1.6" );
+                }
+
+
+                if (ver.startsWith ( "1.5" )) {
+                    v = new BigDecimal ("1.5" );
+                }
+
+
+                if (ver.startsWith ( "1.9" )) {
+                    v = new BigDecimal ("1.9" );
+                }
+
+                b = Integer.parseInt ( split[ 1 ] );
+            } catch ( Exception ex ) {
+                ex.printStackTrace ();
+                System.err.println ( "Unable to determine build number or version" );
+            }
+        } else {
+            v = new BigDecimal ( sversion );
+            b = -1;
+        }
+
+        buildNumber = b;
+        version = v;
+
+        if ( version.compareTo ( new BigDecimal ( "1.7" )) >=0 ) {
+            is1_7OorLater = true;
+        } else {
+            is1_7OorLater = false;
+        }
+
+        if (version.compareTo ( new BigDecimal ( "1.7" ))==0 ){
+              is1_7 = true;
+        } else {
+            is1_7 = false;
+        }
+
+
+        if (version.compareTo ( new BigDecimal ( "1.8" ))==0 ){
+            is1_8 = true;
+        } else {
+            is1_8 = false;
+        }
+    }
+
+
+    public static void println ( String message ) {
+        System.out.println ( message );
+    }
+
+    public static void print ( String message ) {
+        System.out.print ( message );
+    }
+
+
+    public static boolean isWindows () {
+        return isWindows;
+    }
+
+    public static boolean is1_7OrLater () {
+        return is1_7OorLater;
+    }
+
+    public static boolean is1_7() {
+        return is1_7;
+    }
+    public static boolean is1_8() {
+        return is1_8;
+    }
+
+    public static int buildNumber () {
+        return buildNumber;
+    }
+
+    static {
+        if ( System.getProperty ( "os.name" ).contains ( "Windows" ) ) {
             isWindows = true;
         } else {
             isWindows = false;
@@ -38,44 +123,44 @@ public class Sys {
 
     }
 
-    public static char windowsPathSeparator() {
+    public static char windowsPathSeparator () {
         return '\\';
     }
 
 
-    final static AtomicReference<TimeKeeper> timer = new AtomicReference<TimeKeeper>( new TimeKeeperBasic() );
+    final static AtomicReference<TimeKeeper> timer = new AtomicReference<TimeKeeper> ( new TimeKeeperBasic () );
 
-    public static TimeKeeper timer() {
-        return timer.get();
+    public static TimeKeeper timer () {
+        return timer.get ();
     }
 
-    public static long time() {
-        return timer.get().time();
+    public static long time () {
+        return timer.get ().time ();
     }
 
 
     static {
         boolean _inContainer;
-        boolean forceInContainer = Boolean.parseBoolean( System.getProperty( "org.boon.forceInContainer", "false" ) );
-        boolean forceNoContainer = Boolean.parseBoolean( System.getProperty( "org.boon.forceNoContainer", "false" ) );
+        boolean forceInContainer = Boolean.parseBoolean ( System.getProperty ( "org.boon.forceInContainer", "false" ) );
+        boolean forceNoContainer = Boolean.parseBoolean ( System.getProperty ( "org.boon.forceNoContainer", "false" ) );
 
         if ( forceNoContainer ) {
             _inContainer = false;
         } else if ( forceInContainer ) {
             _inContainer = true;
         } else {
-            _inContainer = detectContainer();
+            _inContainer = detectContainer ();
         }
 
         inContainer = _inContainer;
     }
 
-    private static boolean detectContainer() {
+    private static boolean detectContainer () {
 
         boolean _inContainer;
 
         try {
-            Class.forName( "javax.servlet.http.HttpServlet" );
+            Class.forName ( "javax.servlet.http.HttpServlet" );
 
             _inContainer = true;
         } catch ( ClassNotFoundException e ) {
@@ -83,7 +168,7 @@ public class Sys {
         }
         if ( !_inContainer ) {
             try {
-                Class.forName( "javax.ejb.EJBContext" );
+                Class.forName ( "javax.ejb.EJBContext" );
 
                 _inContainer = true;
             } catch ( ClassNotFoundException e ) {
@@ -97,16 +182,14 @@ public class Sys {
     }
 
 
-    public static boolean inContainer() {
+    public static boolean inContainer () {
         return inContainer;
     }
 
 
-
-
     /* Everything that has a cache you need to hold on to, should use this so they can
      * all be stuffed into application context. */
-    public static Object contextToHold() {
+    public static Object contextToHold () {
 
         return Lists.list ( Reflection.contextToHold (), Annotations.contextToHold () );
     }
--- src/main/java/org/boon/core/reflection/FastStringUtils.java
+++ src/main/java/org/boon/core/reflection/FastStringUtils.java
@@ -1,5 +1,6 @@
 package org.boon.core.reflection;
 
+import org.boon.core.Sys;
 import sun.misc.Unsafe;
 
 import java.lang.reflect.Field;
@@ -11,45 +12,59 @@ import java.nio.charset.StandardCharsets;
  */
 public class FastStringUtils {
 
+
     public static final Unsafe UNSAFE;
     public static final long STRING_VALUE_FIELD_OFFSET;
-    public static final boolean HAS_UNSAFE;
+    public static final boolean ENABLED;
 
     private static final boolean WRITE_TO_FINAL_FIELDS = Boolean.parseBoolean( System.getProperty( "org.boon.dont.write.to.final.fields", "false" ) );
+    private static final boolean DISABLE = Boolean.parseBoolean( System.getProperty( "org.boon.faststringutils", "false" ) );
 
     static {
-        Unsafe unsafe;
-        try {
-            Field unsafeField = Unsafe.class.getDeclaredField( "theUnsafe" );
-            unsafeField.setAccessible( true );
-            unsafe = ( Unsafe ) unsafeField.get( null );
 
-        } catch ( Throwable cause ) {
-            unsafe = null;
-
-        }
-
-        UNSAFE = unsafe;
-        HAS_UNSAFE = unsafe != null;
-
-        long stringValueFieldOffset = -1L;
-
-        if ( HAS_UNSAFE ) {
+        if (!DISABLE && is17Build40OrLater() )  {
+            Unsafe unsafe;
             try {
-                stringValueFieldOffset = unsafe.objectFieldOffset( String.class.getDeclaredField( "value" ) );
-            } catch ( Throwable cause ) {
-            }
-        }
-        STRING_VALUE_FIELD_OFFSET = stringValueFieldOffset;
+                Field unsafeField = Unsafe.class.getDeclaredField( "theUnsafe" );
+                unsafeField.setAccessible( true );
+                unsafe = ( Unsafe ) unsafeField.get( null );
 
+            } catch ( Throwable cause ) {
+                unsafe = null;
+
+            }
+
+            UNSAFE = unsafe;
+            ENABLED = unsafe != null;
+
+            long stringValueFieldOffset = -1L;
+
+            if ( ENABLED ) {
+                try {
+                    stringValueFieldOffset = unsafe.objectFieldOffset( String.class.getDeclaredField( "value" ) );
+                } catch ( Throwable cause ) {
+                }
+            }
+            STRING_VALUE_FIELD_OFFSET = stringValueFieldOffset;
+        } else {
+            STRING_VALUE_FIELD_OFFSET = 0;
+            UNSAFE = null;
+            ENABLED = false;
+
+        }
+
+    }
+
+    private static boolean is17Build40OrLater () {
+        return  Sys.is1_8() ||  ( Sys.is1_7() && Sys.buildNumber() >= 40 );
     }
 
     public static boolean hasUnsafe() {
-        return HAS_UNSAFE;
+        return ENABLED;
     }
 
     public static char[] toCharArray( final String string ) {
-        return HAS_UNSAFE ?
+        return ENABLED ?
                 ( char[] ) UNSAFE.getObject( string, STRING_VALUE_FIELD_OFFSET ) :
                 string.toCharArray();
 
@@ -57,7 +72,7 @@ public class FastStringUtils {
 
 
     public static char[] toCharArray( final CharSequence charSequence ) {
-        return HAS_UNSAFE ?
+        return ENABLED ?
                 ( char[] ) UNSAFE.getObject( charSequence.toString(), STRING_VALUE_FIELD_OFFSET ) :
                 charSequence.toString().toCharArray();
 
@@ -65,7 +80,7 @@ public class FastStringUtils {
 
     public static char[] toCharArrayFromBytes( final byte[] bytes, Charset charset ) {
         final String string = new String( bytes, StandardCharsets.UTF_8 );
-        return HAS_UNSAFE ?
+        return ENABLED ?
                 ( char[] ) UNSAFE.getObject( string, STRING_VALUE_FIELD_OFFSET ) :
                 string.toCharArray();
     }
@@ -73,7 +88,7 @@ public class FastStringUtils {
 
     public static String noCopyStringFromChars( final char[] chars ) {
 
-        if ( HAS_UNSAFE && WRITE_TO_FINAL_FIELDS ) {
+        if ( WRITE_TO_FINAL_FIELDS && ENABLED ) {
 
             final String string = new String();
             UNSAFE.putObject( string, STRING_VALUE_FIELD_OFFSET, chars );
