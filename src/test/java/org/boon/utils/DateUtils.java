4c09f9f343414f48f2385b2e29b12edf048b960b
Richard Hightower <richardhightower@gmail.com>
2014-01-07 09:32:04 -0800

    Build was slightly broke for Americans with the last pull. :)

--- src/test/java/org/boon/utils/DateUtils.java
+++ src/test/java/org/boon/utils/DateUtils.java
@@ -3,12 +3,21 @@ package org.boon.utils;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 import java.util.TimeZone;
 
 public class DateUtils {
 
 	public static String getGMTString(Date date) {
-        DateFormat df = new SimpleDateFormat();
+
+        /*
+        * To SL: Now I know what you mean work everywhere. :)
+        * We format our dates differently here.
+        * DAY/MONTH/YEAR although logical and done everywhere. :)
+        * --RMH
+        */
+        DateFormat df =  SimpleDateFormat.getDateTimeInstance( DateFormat.SHORT,
+                DateFormat.SHORT, Locale.FRANCE );
         df.setTimeZone(TimeZone.getTimeZone("GMT"));
         return df.format(date);
 	}
