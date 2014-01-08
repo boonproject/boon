8d1fb62cf8e5b0ea2f1f006993f8c915b1c958ce
javaboon <richard.m.hightower@gmail.com>
2014-01-08 10:39:57 -0800

    added tutorial back

--- src/test/java/org/boon/json/JsonTutorial.java
+++ src/test/java/org/boon/json/JsonTutorial.java
@@ -534,16 +534,16 @@ public class JsonTutorial {
 
     public static void main( String... args ) throws Exception {
 
-//        part1();
-//        part2();
-//        part3();
+        part1();
+        part2();
+        part3();
         part4();
-//        part5();
-//        part6();
-//        part7();
-//        part8();
-//        part9();
-//        part10();
+        part5();
+        part6();
+        part7();
+        part8();
+        part9();
+        part10();
 
 
     }
