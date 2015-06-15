16bc1d6a732bfb8abb9c7d840bff4a172d98f102
Richard Hightower <rhightower@gmail.com>
2015-06-15 11:43:06 -0700

    added a new constructor so you can pass optinos and such to JSON

--- slumberdb/slumberdb-leveldb/src/main/java/org/boon/slumberdb/leveldb/SimpleJsonKeyValueStoreLevelDB.java
+++ slumberdb/slumberdb-leveldb/src/main/java/org/boon/slumberdb/leveldb/SimpleJsonKeyValueStoreLevelDB.java
@@ -2,6 +2,7 @@ package org.boon.slumberdb.leveldb;
 
 
 import org.boon.slumberdb.impl.SimpleJsonKeyValueStore;
+import org.iq80.leveldb.Options;
 
 public class SimpleJsonKeyValueStoreLevelDB<V> extends SimpleJsonKeyValueStore<V> {
 
@@ -9,6 +10,12 @@ public class SimpleJsonKeyValueStoreLevelDB<V> extends SimpleJsonKeyValueStore<V
     public SimpleJsonKeyValueStoreLevelDB(String fileName, Class<V> cls) {
 
         super(new LevelDBKeyValueStore(fileName), cls);
+    }
+
+
+    public SimpleJsonKeyValueStoreLevelDB(String fileName, Options options,  boolean log, Class<V> cls) {
+
+        super(new LevelDBKeyValueStore(fileName, options, log), cls);
     }
 
 }
